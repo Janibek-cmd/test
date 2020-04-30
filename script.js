9db8f7326fc8a9a6cf99b79b1282248491efdc6c
john <jonathan.18.08.98@gmail.com>
2020-04-30 12:02:48 +0500

    Это наш коммит

---
+++ script.js
@@ -0,0 +1,31 @@
+'use strict';
+
+let a = 5;
+console.log(a);
+
+
+// alert("Hello world!");
+// let answer = confirm("Are you here?");
+// console.log(answer);
+//let answer = prompt("Есть ли вам 18?" , "Да");
+
+// console.log(typeof(answer));
+
+// console.log("arr" + " - object");
+// console.log(4 + + " - object");
+
+let incr = 10,
+    decr = 10;
+
+incr++;
+decr--;
+console.log(incr++);
+console.log(decr--);
+
+console.log(5*2);
+console.log("2"==2);
+
+let isCheked = false,
+    isClose  = false;
+ console.log(isCheked || !isClose);
+
